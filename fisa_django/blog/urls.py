176b72453f76aef1a12cc0c9eb9f54c5800c58ee
kimnagyeong99 <knk990928@gmail.com>
2024-08-27 17:00:04 +0900

    0827_views_update

--- fisa_django/blog/urls.py
+++ fisa_django/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index), # localhost:8000/blog 경로, 경로를 호출하면 실행할 함수의 위치 
     path('post_list', views.PostList.as_view()),
     path('about_me', views.about_me),
+    path('<int:pk>', views.PostDetail.as_view()), # <자료형:필드명>
+    path('create-post/', views.PostCreate.as_view())
 ]
--- fisa_django/blog/views.py
+++ fisa_django/blog/views.py
@@ -1,6 +1,38 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ["title", "content", "head_image"]
+
+    # CreateView가 내장한 함수 - 오버라이딩 
+    # tag는 참조관계이므로 Tag 테이블에 등록된 태그만 쓸 수 있는 상황
+    # 임의로 방문자가 form에 Tag를 달아서 보내도록 form_valid()에 결과를 임시로 담아두고
+    # 저장된 포스트로 돌아오도록 
+    # def form_valid(self, form):
+    #     current_user = self.request.user
+    #     if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+    #         form.instance.author = current_user
+    #         response = super(PostCreate, self).form_valid(form)
+
+    #         tags_str = self.request.POST.get('tags_str')
+    #         if tags_str:
+    #             tags_str = tags_str.strip() # 책, 독후감, 작가명
+
+    #             tags_str = tags_str.replace(',', ';')
+    #             tags_list = tags_str.split(';')
+
+    #             for t in tags_list:
+    #                 t = t.strip()
+    #                 tag, is_tag_created = Tag.objects.get_or_create(tag_name=t)
+    #                 if is_tag_created:
+    #                     tag.slug = slugify(t, allow_unicode=True)
+    #                     tag.save()
+    #                 self.object.tag.add(tag)
+
 
 class PostList(ListView):   # post_list.html, post-list
     model = Post 
@@ -18,14 +50,16 @@ def index(request): # 함수를 만들고, 그 함수를 도메인 주소 뒤에
         } # 없는 index.html을 호출하고 있음
     )
 
-# class about_me(ListView):   # post_list.html, post-list
-#     model = Post 
-#     template_name = 'blog/about_me.html' 
-#     # ordering = '-pk' 
-#     context_object_name = 'Single_pages/about_me'
-
 def about_me(request): # 함수를 만들고, 그 함수를 도메인 주소 뒤에 달아서 호출하는 구조
     return render(
         request,
         'blog/about_me.html'
     )
+
+class PostDetail(DetailView):
+    model = Post 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = '임의로 작성한 새로운 변수'
+        return context
